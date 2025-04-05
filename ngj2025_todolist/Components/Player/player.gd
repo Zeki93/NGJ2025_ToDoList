dab38e49e062786b9fd6c446944d16197f237e35
Zeki93 <Zeki1982@live.com>
2025-04-05 14:04:13 +0200

    Add sleepcontroller

--- ngj2025_todolist/Components/Player/player.gd
+++ ngj2025_todolist/Components/Player/player.gd
@@ -8,26 +8,26 @@ var acceleration = Vector3(0.0, -gravity, 0.0)
 @export var air_drag = 2
 
 func _ready():
-    Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta):
-    
-    velocity += acceleration * delta
+	
+	velocity += acceleration * delta
 
-    var drag_factor
-    if is_on_floor():
-        drag_factor = ground_drag * delta
-    else:
-        drag_factor = air_drag * delta
-    drag_factor = clampf(1.0 - drag_factor, 0.0, 1.0)
-    velocity *= drag_factor
-    
-    move_and_slide()
+	var drag_factor
+	if is_on_floor():
+		drag_factor = ground_drag * delta
+	else:
+		drag_factor = air_drag * delta
+	drag_factor = clampf(1.0 - drag_factor, 0.0, 1.0)
+	velocity *= drag_factor
+	
+	move_and_slide()
 
 func _on_failed_event():
-    playerController.exitPuzzle();
-    pass # Replace with function body.
+	playerController.exitPuzzle();
+	pass # Replace with function body.
 
 func _on_succesful_event():
-    playerController.exitPuzzle();
-    pass # Replace with function body.
+	playerController.exitPuzzle();
+	pass # Replace with function body.
